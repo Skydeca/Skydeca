adf01c8681264f7be03b67387332a6dcf383e3d9
C. J. Murray <cj@skydeca.com>
2025-07-05 11:05:59 -0500

    fix: removed gap around word cloud and increased spread

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,7 +132,7 @@ export default function HomePage() {
         </motion.section>
 
         {/* 🌥 Word Cloud */}
-        <div className="relative h-[40rem] w-full mt-8 overflow-hidden">
+        <div className="relative h-[40rem] w-full overflow-hidden">
           {wordCloudTags.map((word, i) => (
             <motion.div
               key={i}
